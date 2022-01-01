64b28221cfcd1e556583fb31736a44540f1ee099
Robert Mengual <robert.mengual@outlook.com>
2022-01-01 19:27:43 +0100

    first version

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,94 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <div>
+      <button
+        v-for="index in [...Array(5).keys()]"
+        v-on:click="setColumns(index + 1)"
+        v-bind:key="index"
+      >
+        {{ index + 1 }}
+      </button>
+    </div>
+
+    <div
+      id="questions-table"
+      v-bind:style="{ 'grid-template-columns': tableColumns }"
+    >
+      <div
+        class="question"
+        v-for="(question, index) in questions"
+        v-bind:key="index"
+      >
+        <div class="image">
+          <img v-bind:src="question.imageUri" />
+        </div>
+        <div class="input">
+          <label>{{ index + 1 }} - </label><input />
+          <button v-on:click="checkAnswer('', index)">V</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { questions } from "./questions";
+import JSEncrypt from "jsencrypt";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
+  data: () => {
+    return {
+      questions,
+      tableColumns: "repeat(4, 1fr)",
+    };
+  },
+  methods: {
+    setColumns: function (numberOfColumns: number) {
+      this.tableColumns = `repeat(${numberOfColumns}, 1fr)`;
+    },
   },
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+#questions-table {
+  display: grid;
+  gap: 24px;
+  row-gap: 36px;
+  width: 80%;
+  margin: auto;
+}
+
+@media (max-width: 500px) {
+  #questions-table {
+    width: 100%;
+  }
+}
+
+.question {
+  max-width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.question > .image {
+  max-width: 100%;
+  height: 0;
+  padding-bottom: 100%;
+  border: 1px solid gray;
+}
+
+.question img {
+  max-width: 100%;
+}
+
+.question > .input {
+  margin-top: 12px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.input input {
+  width: 75%;
 }
 </style>
---
+++ src/questions.ts
@@ -0,0 +1,84 @@
+export const questions: { imageUri: string; solutions: string[] }[] = [
+  {
+    imageUri: "http://localhost:8080/Amaral.jpg",
+    solutions: [
+      "MIfOYsku8rzvreLSZ9nOmZVEpndRnIq/6c0k3Kbgu6e12dv+w1KpZ6DgAXV0YfGiIiVxtyt8L6mGbzgyQLEz3gJGezwsIuWkohYrplCPNAxJweyw8AGplr9MG6rcn9wTMxeSR+9tPvvM79BrXVgQt71HZ0j8DC8+1HwmG7Z0ZpA=",
+    ],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+  {
+    imageUri: "",
+    solutions: [],
+  },
+];
