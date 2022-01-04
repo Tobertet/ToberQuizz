c8339e6fa6b78e8fcaee7393eab4b5d360082eef
Robert Mengual <robert.mengual@outlook.com>
2022-01-04 22:23:09 +0100

    Add android platform

---
+++ android/app/src/main/java/com/robertmengual/toberquizz/MainActivity.java
@@ -0,0 +1,5 @@
+package com.robertmengual.toberquizz;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.robertmengual.toberquizz',
+  appName: 'ToberQuizz',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
