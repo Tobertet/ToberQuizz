aa691dcc1d008ec8e3fa3f892a7ddf667a5ea53d
Robert Mengual <robert.mengual@outlook.com>
2022-01-10 10:59:24 +0100

    Break Challenge component into multiple smaller ones

---
+++ src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer>
+    <hr />
+    <p>
+      Esta aplicación es tecnológicamente ética y no contiene ni anuncios ni
+      trackers ni cookies, respetando así totalmente tu privacidad. Además, es
+      open-source y puedes encontrar su código
+      <a
+        alt="link to the source code in Github"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/Tobertet/ToberQuizz"
+        >aquí</a
+      >.
+    </p>
+    <p>
+      Si te ha gustado, no dudes en compartirlo libremente con quien quieras.
+      Si, además quieres ayudarme a hacer más contenido tecnológicamente ético
+      como este, puedes
+      <a
+        alt="link to donation page"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://Ko-fi.com/robertmengual"
+        >invitarme a un café</a
+      >.
+    </p>
+    <p>Hecho para ti con mucho ❤️ por Robert Mengual.</p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Footer",
+});
+</script>
+
+<style lang="scss">
+footer {
+  margin-top: 24px;
+  margin-bottom: 48px;
+  p {
+    font-size: 1.2rem;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <pre id="ascii-title">
+ _______    _                ____        _         
+|__   __|  | |              / __ \      (_)        
+   | | ___ | |__   ___ _ __| |  | |_   _ _ ________
+   | |/ _ \| '_ \ / _ \ '__| |  | | | | | |_  /_  /
+   | | (_) | |_) |  __/ |  | |__| | |_| | |/ / / / 
+   |_|\___/|_.__/ \___|_|   \___\_\\__,_|_/___/___|
+    </pre>
+    <p>
+      Te doy la bienvenida a ToberQuizz, la aplicación tecnológicamente ética
+      donde encontrarás desafíos originales y gratuitos que pondrán en jaque tu
+      imaginación.
+    </p>
+    <h2>{{ title }}</h2>
+    <p>
+      {{ description }}
+    </p>
+    <p>¿Eres capaz de adivinar todos?</p>
+    <hr />
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+  props: {
+    title: String,
+    description: String,
+  },
+});
+</script>
+
+<style lang="scss">
+header {
+  margin-top: 24px;
+  margin-bottom: 24px;
+  #ascii-title {
+    text-align: center;
+    font-size: 2vw;
+    @media (min-width: 1024px) {
+      font-size: 200%;
+    }
+  }
+  p {
+    font-size: 1.2rem;
+  }
+}
+</style>
