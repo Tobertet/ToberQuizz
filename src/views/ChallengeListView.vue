d48f441f38f75fc5b9a027caf4d243384a4a8878
Robert Mengual <robert.mengual@outlook.com>
2022-01-23 23:45:19 +0100

    refactor challenge list view

--- src/views/ChallengeListView.vue
+++ src/views/ChallengeListView.vue
@@ -1,40 +1,39 @@
 <template>
-  <header>
-    <Banner />
-    <hr />
-  </header>
-  <main>
-    <button
-      :to="$route.params.countryCode"
-      v-for="challenge of Object.keys(challenges)"
-      :key="challenge"
-      @click="goToChallenge(challenge)"
-    >
-      Challenge {{ parseInt(challenge) + 1 }}
-    </button>
-  </main>
-  <Footer />
+  <div id="app-container">
+    <Header />
+    <main>
+      <button
+        :to="$route.params.countryCode"
+        v-for="challengeIndex of Object.keys(challenges)"
+        :key="challengeIndex"
+        @click="goToChallenge(parseInt(challengeIndex) + 1)"
+      >
+        Challenge {{ parseInt(challengeIndex) + 1 }}
+      </button>
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Banner from "@/components/Banner.vue";
+import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import { QUIZZ_DATA } from "@/quizzData";
 import { Challenge, CountryCodes } from "@/models";
 
 export default defineComponent({
   name: "ChallengeList",
-  components: { Banner, Footer },
+  components: { Header, Footer },
   data: function () {
     return {
       challenges: new Array<Challenge>(),
     };
   },
   methods: {
-    goToChallenge: function (challengeIndex: number) {
+    goToChallenge: function (challengeNumber: number) {
       this.$router.push({
-        path: `/${this.$route.params.countryCode}/${challengeIndex}`,
+        path: `/${this.$route.params.countryCode}/${challengeNumber}`,
       });
     },
   },
