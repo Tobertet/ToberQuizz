311b77523fe0a74665a61deebca7dc37b5329666
Robert Mengual <robert.mengual@outlook.com>
2022-02-01 15:48:35 +0100

    refactor styles

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import { i18n } from "./i18n";
 
-import "./assets/main.css";
+import "./styles/main.css";
 
 createApp(App).use(i18n).use(router).mount("#app");
