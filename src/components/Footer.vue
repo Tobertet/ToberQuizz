a2df6266eb21db80a05d962a045da790890218f9
Robert Mengual <robert.mengual@outlook.com>
2022-02-02 14:53:10 +0100

    add contribution p to the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,16 @@
   <footer>
     <hr />
     <p>
+      ToberQuizz es totalmente gratuito y su misión es puramente solidaria.
+      <a
+        class="regular"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://robertmengual.com/projects;project=toberquizz"
+        >¿Quieres saber por qué y cómo contribuir?</a
+      >
+    </p>
+    <p>
       Hecho para ti con mucho ❤️ por
       <span class="regular">Robert Mengual</span>.
     </p>
@@ -14,6 +24,11 @@ footer {
   margin-bottom: 24px;
   p {
     font-size: 18px;
+  }
+  a {
+    color: var(--warning-color);
+    font-size: 18px;
+    cursor: pointer;
   }
 }
 </style>
