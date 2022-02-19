b983527371fac311b5455eb98bcd915ece985e77
Robert Mengual <robert.mengual@outlook.com>
2022-02-19 16:20:07 +0100

    add webHashHistory to the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import ChallengeView from "@/views/ChallengeView.vue";
 import ChallengeListView from "@/views/ChallengeListView.vue";
@@ -19,7 +19,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
