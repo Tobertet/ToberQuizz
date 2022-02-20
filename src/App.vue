2c5d1bb3fa1f04f94f478db3c9f2849fb0300d15
Robert Mengual <robert.mengual@outlook.com>
2022-02-20 17:35:07 +0100

    Override android native back button

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+import { App } from "@capacitor/app";
+
+export default defineComponent({
+  setup: () => {
+    const router = useRouter();
+
+    App.addListener("backButton", () => {
+      if (router.currentRoute.value.fullPath === "/") {
+        App.exitApp();
+      } else {
+        router.back();
+      }
+    });
+  },
+});
+</script>
