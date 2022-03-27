dae44ce67a5396d4c19058898aa352445cf27ae3
MartaBallesterB <martaballesterbadenes@gmail.com>
2022-03-27 14:17:28 +0100

    crear desafío 4

---
+++ src/quizzData/ES/challenges/4.ts
@@ -0,0 +1,140 @@
+import { Challenge } from "@/models";
+
+export const CHALLENGE_4: Challenge = {
+  description: `El librero libera un libro de la librería, liberando de la librería un libro del librero.
+
+Probablemente, la frase anterior no tenga ningún sentido. Pero lo que no es probable es que este mes el desafío va sobre una de nuestras más valiosas joyas: la literatura.
+
+Cada imagen representa, de alguna forma, un gran libro de nuestra queridísima literatura española.
+
+NOTA: Puede haber sido, o no, liberado de la librería del librero.`,
+  startingDate: "",
+  questions: [
+    {
+      solutions: [
+        "ecf6716ce82b61cfde3789b24f9392dbdbf4854b0274aa50ae75ade35345b12c",
+      ],
+    },
+    {
+      solutions: [
+        "aa17bea341015554ccb89951584e0f7965970db0dca06153c07b0d813b64eab4",
+      ],
+    },
+    {
+      solutions: [
+        "e05ee26e21e71ebe7b4594ab5f138019379dd9cd080d77b58f7db0512c72a75e",
+        "7aaf4c4f7b233c6d949075bdb3307d1481f533be3793fff0cbf1ae7e4c560357",
+        "077d3b92615e2b855151b426bb03d5ca83c38aa48e1ecca6496b245354958c4e",
+      ],
+    },
+    {
+      solutions: [
+        "621c0ac9ce9c9eb5a5ac9c7fc6cbbef20c9a5dbedf34f15ebf98a98af8d15a1e",
+      ],
+    },
+    {
+      solutions: [
+        "6c4373e6f01ee76b59cb7998cd7418208084c0866c47d8b6aeaca454c421fd14",
+        "7c2c00c94eaf6693922d1a0b9f680acf0fc335b2c1a1393e408ca07a6feb7cb5",
+      ],
+    },
+    {
+      solutions: [
+        "2b57ddb2258087ec89f0b756bb530d77924f1571a5737336619a72b8b85c8701",
+      ],
+    },
+    {
+      solutions: [
+        "1a32c7e929550eebd12eee387174ef09415b29d0d1f3062d796f1d1196d33f4b",
+      ],
+    },
+    {
+      solutions: [
+        "fe7020b0f99b3e5c20b579b4d379099fb6e0cfb7afbbb0d1af84def75ec7b0fe",
+        "abb4a7bfff031aed8a99b2365ceaeb6b94d2b6db3f74c155c1af41ac1e29f6ee",
+      ],
+    },
+    {
+      solutions: [
+        "73852169d38d98394b6196e252390516de5202444e2891dccef51db8d0bed62f",
+      ],
+    },
+    {
+      solutions: [
+        "473899da5c9a153f6c0447eca5288a2b2f452c60a57d60119fd91a6b1174767a",
+      ],
+    },
+    {
+      solutions: [
+        "a6c81c90225e7ac3e746040cbede63288833a4246007fab0b4d1966ae6eced60",
+      ],
+    },
+    {
+      solutions: [
+        "978f4fe08310bbc56a16d723a64199db304e9f19949cdc2d76d9809958cd2d01",
+      ],
+    },
+    {
+      solutions: [
+        "fe60f31e1a96138174c0858b04bf5d1e38326d643916ba374843e006be9760a2",
+      ],
+    },
+    {
+      solutions: [
+        "8cbbc65901dba0606af6b3ed5c43a3a6f5d2bdd40cbc17a03691532eb8723e65",
+      ],
+    },
+    {
+      solutions: [
+        "bd2e2afac764b0dfa90cbff0dd61c2be58748d128ad363e4b68e0c1f86751026",
+      ],
+    },
+    {
+      solutions: [
+        "9d9d8700fe78566ba0eef3a947f597f2507c7b47eb64019dd775620694907dde",
+      ],
+    },
+    {
+      solutions: [
+        "b556ee9ef01d01a4732695c432c83f71d725f5c00f39cf235d5b7c18c86ec8c5",
+      ],
+    },
+    {
+      solutions: [
+        "027f6090c5144188248d5b880ac68f138fbb68253ceeccefd31f6ff5f6b7b75a",
+        "dce00527178e75e6e60a5fba4a0e9cb2ab3b1e85f12f6630db6dfc8dcc0e715c",
+      ],
+    },
+    {
+      solutions: [
+        "0b786ee855a91950f95903082cbce4883d8f91ecc6da201cf743486121bf1871",
+      ],
+    },
+    {
+      solutions: [
+        "006d1a19a9c06ef6ebca10f9252681db8b80c846df7668e3dfbf65183a475bff",
+        "b0a3025be0cf47223d837c94936c419fdb513153615b5b41d500a2911b262fed",
+      ],
+    },
+    {
+      solutions: [
+        "aaf3121e8f851294dd71986bdacadca7363b3bdea1db062c01a55f50dbe4f07e",
+      ],
+    },
+    {
+      solutions: [
+        "42b5c8d0f42b63905a78dd138fd6129136bf4bb2ffd06d0795e30c282c1d8ea9",
+      ],
+    },
+    {
+      solutions: [
+        "5705d7c3e4892721f386ff09a4bd2c19f2c7f698a2f43d4831d8e9065fb38c62",
+      ],
+    },
+    {
+      solutions: [
+        "d8f9badc6f4690df5dbfae049c7e42199aaed7b502fb4dbde911aeeb2c7d24de",
+      ],
+    },
+  ],
+};
--- src/quizzData/ES/index.ts
+++ src/quizzData/ES/index.ts
@@ -1,6 +1,11 @@
 import { Challenge } from "@/models";
-import { CHALLENGE_1, CHALLENGE_2, CHALLENGE_3 } from "./challenges";
+import { 
+    CHALLENGE_1, 
+    CHALLENGE_2, 
+    CHALLENGE_3, 
+    CHALLENGE_4 
+} from "./challenges";
 
-const ES: Challenge[] = [CHALLENGE_1, CHALLENGE_2, CHALLENGE_3];
+const ES: Challenge[] = [CHALLENGE_1, CHALLENGE_2, CHALLENGE_3, CHALLENGE_4];
 
 export default ES;
