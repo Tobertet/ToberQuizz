ab56cd02c10e1a84c2c3fde603bba9d8f6576e3f
Robert Mengual <robert.mengual@outlook.com>
2022-04-02 14:49:11 +0200

    create e2e tests for challenge view

---
+++ tests/e2e/specs/Challenge.js
@@ -0,0 +1,7 @@
+describe("Challenge view", () => {
+  it("shows the questions for that challenge", () => {
+    cy.visit("#/ES/1");
+
+    cy.get("#questions-table").find(".question").should("have.length", 30);
+  });
+});
