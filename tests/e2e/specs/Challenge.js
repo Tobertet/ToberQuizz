7ca13e3d9c1e98b61acfaebf9efe5de033ea72bd
Robert Mengual <robert.mengual@outlook.com>
2022-04-02 16:14:34 +0200

    Added an e2e test for stored answers on challenge view

--- tests/e2e/specs/Challenge.js
+++ tests/e2e/specs/Challenge.js
@@ -4,4 +4,22 @@ describe("Challenge view", () => {
 
     cy.get("#questions-table").find(".question").should("have.length", 3);
   });
+  describe("when there are stored answers", () => {
+    it("autocompletes the answered questions", () => {
+      const storedValues = ["test answer 1", null, "whatever"];
+
+      localStorage.setItem(
+        "CapacitorStorage.ES_1",
+        JSON.stringify(storedValues)
+      );
+
+      cy.visit("#/ES/1");
+
+      cy.get(".question").each((item, index) => {
+        cy.wrap(item)
+          .find("input")
+          .should("have.value", storedValues[index] || "");
+      });
+    });
+  });
 });
