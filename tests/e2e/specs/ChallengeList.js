cd283dc9cc9093b2d48dd828bb0b62f3befcac01
Robert Mengual <robert.mengual@outlook.com>
2022-04-02 14:49:02 +0200

    Create e2e tests for challenge list view

---
+++ tests/e2e/specs/ChallengeList.js
@@ -0,0 +1,31 @@
+describe("Challenge list view", () => {
+  it("contains the challenges for that country", () => {
+    cy.visit("#/ES");
+    cy.contains("Challenge 1");
+    cy.contains("Challenge 2");
+    cy.contains("Challenge 3");
+    cy.contains("Challenge 4");
+  });
+  describe("when a challenge is clicked", () => {
+    it("navigates to the challenge page", () => {
+      cy.visit("#/ES");
+
+      cy.contains("Challenge 2").click();
+
+      cy.url().should("include", "/ES/2");
+    });
+  });
+  describe("when 'Why ToberQuizz?' is clicked", () => {
+    it("navigates to the ToberQuizz page in Robert's website", () => {
+      cy.visit("#/ES");
+
+      cy.contains("Why ToberQuizz?")
+        .should("have.attr", "target")
+        .and("eq", "_blank");
+
+      cy.contains("Why ToberQuizz?")
+        .should("have.attr", "href")
+        .and("eq", "https://robertmengual.com/projects;project=toberquizz");
+    });
+  });
+});
