8a8ead421d48a6fc793cbdf38db9302f1b365bc3
Robert Mengual <robert.mengual@outlook.com>
2022-04-24 16:48:44 +0200

    Added Answer service and made answer immutable

--- src/domain/models/Answer.ts
+++ src/domain/models/Answer.ts
@@ -1,18 +1,31 @@
-export type CorrectAnswer = {
+export type CorrectAnswer = Readonly<{
   text: string;
   isCorrect: true;
-};
+}>;
 
-export type IncorrectAnswer = {
+export type IncorrectAnswer = Readonly<{
   text: string;
   isCorrect: false;
-};
+}>;
 
 export type CheckedAnswer = CorrectAnswer | IncorrectAnswer;
 
-export type UncheckedAnswer = {
+export type UncheckedAnswer = Readonly<{
   text: string;
   isCorrect?: never;
-};
+}>;
 
 export type Answer = CheckedAnswer | UncheckedAnswer;
+
+export type CheckAnswer = (
+  answer: UncheckedAnswer,
+  isCorrect: boolean
+) => CheckedAnswer;
+
+export type CheckAnswerCorrectly = (answer: UncheckedAnswer) => CorrectAnswer;
+
+export type CheckAnswerIncorrectly = (
+  answer: UncheckedAnswer
+) => IncorrectAnswer;
+
+export type UncheckAnswer = (answer: CheckedAnswer) => UncheckedAnswer;
---
+++ src/domain/services/AnswerService.ts
@@ -0,0 +1,54 @@
+import {
+  Answer,
+  CheckAnswer,
+  CheckAnswerCorrectly,
+  CheckAnswerIncorrectly,
+  CheckedAnswer,
+  CorrectAnswer,
+  IncorrectAnswer,
+  UncheckAnswer,
+  UncheckedAnswer,
+} from "@/domain";
+
+const check: CheckAnswer = (answer, isCorrect) => ({
+  ...answer,
+  isCorrect,
+});
+
+const checkCorrectly: CheckAnswerCorrectly = (answer) => ({
+  ...answer,
+  isCorrect: true,
+});
+
+const checkIncorrectly: CheckAnswerIncorrectly = (answer) => ({
+  ...answer,
+  isCorrect: false,
+});
+
+const uncheck: UncheckAnswer = (answer) => ({
+  ...answer,
+  isCorrect: undefined,
+});
+
+const isUnchecked = (answer: Answer): answer is UncheckedAnswer =>
+  answer.isCorrect === undefined;
+
+const isChecked = (answer: Answer): answer is CheckedAnswer =>
+  answer.isCorrect !== undefined;
+
+const isCorrect = (answer: Answer): answer is CorrectAnswer =>
+  isChecked(answer) && answer.isCorrect === true;
+
+const isIncorrect = (answer: Answer): answer is IncorrectAnswer =>
+  isChecked(answer) && answer.isCorrect === false;
+
+export const AnswerService = {
+  check,
+  checkCorrectly,
+  checkIncorrectly,
+  uncheck,
+  isUnchecked,
+  isChecked,
+  isCorrect,
+  isIncorrect,
+};
